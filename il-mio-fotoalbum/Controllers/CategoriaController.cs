f211d529473d61718ddc7d304fd50a6d7efa01eb
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-05-27 18:08:32 +0200

    feat: create su Categoria

---
+++ il-mio-fotoalbum/Controllers/CategoriaController.cs
@@ -0,0 +1,35 @@
+﻿using il_mio_fotoalbum.Data;
+using il_mio_fotoalbum.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace il_mio_fotoalbum.Controllers
+{
+    public class CategoriaController : Controller
+    {
+
+        //INDEX
+        public IActionResult Index()
+        {
+           return View(FotoManger.GetCategorie());
+        }
+
+
+        //CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", categoria);
+            }
+
+            CategoriaManager.InsertCategoria(categoria);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ il-mio-fotoalbum/Data/CategoriaManager.cs
@@ -0,0 +1,14 @@
+﻿using il_mio_fotoalbum.Models;
+
+namespace il_mio_fotoalbum.Data
+{
+    public class CategoriaManager
+    {
+        public static void InsertCategoria(Categoria categoria)
+        {
+            using var db = new FotoContext();
+            db.Categorias.Add(categoria);
+            db.SaveChanges();
+        }
+    }
+}
